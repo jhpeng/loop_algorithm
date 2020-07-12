3766b400093bd23cbcca8bab72afd76bcd5cf24e
jhpeng <alanpeng1995@gmail.com>
2020-07-12 15:17:23 +0800

    create table

---
+++ loop/table.c
@@ -0,0 +1,70 @@
+#include "table.h"
+
+table* talbe_alloc(int scale){
+    int size = 1<<scale;
+    
+    table* t = (table*)malloc(sizeof(table));
+    if(t==NULL){
+        printf("table_alloc : memory allocating fail!\n");
+        exit(1);
+    }
+    t->list = (item*)malloc(sizeof(item)*size);
+    if(t->list==NULL){
+        printf("table_alloc : memory allocating fail!\n");
+        exit(1);
+    }
+    
+    t->size = size;
+    t->n = 0;
+    t->key = 0;
+    for(int i=0;i<size;++i)
+        t->list[i].key = UINT64_MAX;
+
+    return t;
+}
+
+void table_free(table* t){
+    free(t->list);
+    free(t);
+}
+
+void table_realloc(table* t){
+    int size = 2*(t->size);
+
+    item* it = (item*)malloc(sizeof(item)*size);
+    if(it==NULL){
+        printf("table_realloc : memory allocating fail!\n");
+        exit(1);
+    }
+
+    for(int i=0;i<size;++i)
+        it[i].key = UINT64_MAX;
+
+    for(int i=0;i<(t->size);++i)
+        it[i] = t->list[i];
+
+    free(t->list);
+    t->list = it;
+    t->size = size;
+}
+
+int table_hash(table* t, uint64_t key){
+    return (int)(key&(t->size-1));
+}
+
+uint64_t table_generate_key(table* t){
+    int i,check=1;
+    while(check){
+        ++(t->key);
+        i = table_hash(t,t->key);
+        if(t->list[i].key==UINT64_MAX) check=0;
+    }
+
+    return t->key;
+}
+
+item* table_search_from_key(table* t, uint64_t key){
+    int i = table_hash(t,key);
+
+    return &(t->list[i]);
+}
---
+++ loop/table.h
@@ -0,0 +1,41 @@
+#ifndef table_h
+#define table_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+#define NSPIN_MAX 16
+
+typedef struct item{
+    uint64_t key;
+    int flag;
+    int type;
+    int nspin;
+    int state[NSPIN_MAX];
+    uint64_t link_key[NSPIN_MAX];
+    int link_spin[NSPIN_MAX];
+} item;
+
+typedef struct table{
+    int size;
+    int n;
+    uint64_t key;
+    item* list;
+} table;
+
+
+table* talbe_alloc(int scale);
+
+void table_free(table* t);
+
+void table_realloc(table* t);
+
+int table_hash(table* t, uint64_t key);
+
+uint64_t table_generate_key(table* t);
+
+item* table_search_from_key(table* t, uint64_t key);
+
+#endif
